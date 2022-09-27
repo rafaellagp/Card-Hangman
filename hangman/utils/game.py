189c9253a7da6b10982029532c0c6132ff44d474
Rafaella Porto <rafaellaporto86@gmail.com>
2022-09-27 16:15:57 +0200

    Update game.py

--- hangman/utils/game.py
+++ hangman/utils/game.py
@@ -1,10 +1,9 @@
-
 import random
 
 class Hangman():
     possible_words = ['becode', 'learning', 'mathematics', 'sessions']
     
-    #Play method
+    
     def __init__(self):
         self.word_to_find = list(random.choice(Hangman.possible_words))
         
@@ -16,6 +15,14 @@ class Hangman():
         self.correctly_guessed_letters = len(self.word_to_find) * ['_']
     
     def play(self):
+        '''
+        A method that asks the player to enter a letter.
+        The player function don't allow type something else than a letter, 
+        and not more than a letter. If the player guessed a letter well, 
+        will add it to the well_guessed_letters list. If not, then will add it 
+        to the wrongly_guessed_letters list and add 1 to error_count.
+        '''
+        
         letter_guess = input("Enter your letter : ")
             
         if len(letter_guess) != 1:
@@ -40,7 +47,11 @@ class Hangman():
         
     #Start_game method 
     def start_game(self):
-        ''' Starts the game .....'''            
+        ''' Starts the method will call play() until the game is over 
+        (because the use guessed the word or because of a game over). 
+        After that will call game_over() if lives is equal to 0 and 
+        the well_played() method if all the letter are guessed.
+        '''            
 
         while True:
             if self.word_to_find == self.correctly_guessed_letters:
